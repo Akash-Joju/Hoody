43b886f9c4824ac4f95fe4ccad4cb355a5a2213b
Akash-Joju <133322595+Akash-Joju@users.noreply.github.com>
2025-07-22 18:44:27 +0530

    adding domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['sportscentral.ph'],
+    domains: ['sportscentral.ph', 'cdn.shopify.com',
+      'images.unsplash.com',
+      'res.cloudinary.com',
+      'static.nike.com',
+      'assets.adidas.com',
+      'media.graphassets.com',
+      'lh3.googleusercontent.com',
+      'images.pexels.com',
+      'i.ibb.co',
+      'firebasestorage.googleapis.com'],
   },
 };
 
